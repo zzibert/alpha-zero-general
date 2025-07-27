cefc055678695d1ad11ee95cbcc229d79460b3ee
Žan Žibert <zzibert@plume.com>
2025-07-27 18:08:17 +0200

    Working on DirectionCondition

--- quoridor/QuoridorLogic.py
+++ quoridor/QuoridorLogic.py
@@ -11,6 +11,66 @@ Squares are stored and manipulated as (x,y) tuples.
 x is the column, y is the row.
 '''
 
+from enum import Enum
+
+class Position():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def isWithinBounds(self):
+        self.x >= 0 & self.x < 17 & self.y >= 0 & self.y < 17
+
+class RelativePosition():
+    def __init__(self, dx, dy):
+        self.dx = dx
+        self.dy = dy
+
+    def transform(self, position):
+        return Position(
+            x=position.x + self.dx,
+            y=position.y + self.dy
+        )
+    
+class DirectionCondition():
+    def __init__(self, occupied, not_occupied, blocked, not_blocked):
+        self.occupied = occupied
+        self.not_occupied = not_occupied
+        self.blocked = blocked
+        self.not_blocked = not_blocked
+
+    def is_condition_met(self, player_position, opponent_position, occupancy):
+        raise NotImplementedError("Subclasses must implement move()")
+    
+    def is_direction_blocked(self, relative, player_position, occupancy):
+        
+        
+
+        
+
+class Direction():
+    def __init__(self, name, relative_position):
+        self.name = name
+        self.relative_position = relative_position
+
+    def 
+
+
+
+
+class Player():
+    def __init__(self, position, tiles):
+        self.position = position
+        self.tiles = tiles
+
+    def update_position(self, position):
+        self.position = position
+
+    def decrement_tiles(self):
+        self.tiles = self.tiles - 1
+
+
+
 class Board():
 
     # All possible directions
@@ -38,25 +98,25 @@ class Board():
 
     # map direction to (x,y) offsets
     _directionToOffset = {
-        "Right": (0, 2),                 # Right
-        "Right-Jump": (0, 4),            # Right-Jump
-        "Right-Jump-Up": (-2, 2),        # Right-Jump-Up
-        "Right-Jump-Down": (2, 2),       # Right-Jump-Down
+        "Right": (0, 2),
+        "Right-Jump": (0, 4),
+        "Right-Jump-Up": (-2, 2), 
+        "Right-Jump-Down": (2, 2),
 
-        "Down": (2, 0),                  # Down
-        "Down-Jump": (4, 0),             # Down-Jump
-        "Down-Jump-Left": (2, -2),       # Down-Jump-Left
-        "Down-Jump-Right": (2, 2),       # Down-Jump-Right
+        "Down": (2, 0),
+        "Down-Jump": (4, 0),
+        "Down-Jump-Left": (2, -2),   
+        "Down-Jump-Right": (2, 2),
 
-        "Left": (0, -2),                 # Left
-        "Left-Jump": (0, -4),            # Left-Jump
-        "Left-Jump-Up": (-2, -2),        # Left-Jump-Up
-        "Left-Jump-Down": (2, -2),       # Left-Jump-Down
+        "Left": (0, -2),
+        "Left-Jump": (0, -4),
+        "Left-Jump-Up": (-2, -2),
+        "Left-Jump-Down": (2, -2),
 
-        "Up": (-2, 0),                   # Up
-        "Up-Jump": (-4, 0),              # Up-Jump
-        "Up-Jump-Left":  (-2, -2),       # Up-Jump-Left
-        "Up-Jump-Right": (-2, 2),        # Up-Jump-Right
+        "Up": (-2, 0),
+        "Up-Jump": (-4, 0),
+        "Up-Jump-Left":  (-2, -2),
+        "Up-Jump-Right": (-2, 2),
     }
 
     def __init__(self):
